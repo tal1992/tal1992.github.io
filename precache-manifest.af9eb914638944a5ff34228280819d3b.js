99ac0bd4d64567556b6a598d979289ef3a841ad7
Tal <talvindersingh1992@gmail.com>
2019-09-22 00:16:19 +0530

    Updates

---
+++ precache-manifest.af9eb914638944a5ff34228280819d3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736e5be9e492afaeb2feb05d98897e23",
+    "url": "/supersquad/index.html"
+  },
+  {
+    "revision": "f2c058d166195f3ae686",
+    "url": "/supersquad/static/css/main.ba4f52f4.chunk.css"
+  },
+  {
+    "revision": "2f29527a01db8285d951",
+    "url": "/supersquad/static/js/2.fe6fdac9.chunk.js"
+  },
+  {
+    "revision": "f2c058d166195f3ae686",
+    "url": "/supersquad/static/js/main.060a0f34.chunk.js"
+  },
+  {
+    "revision": "d132b4573629358daa44",
+    "url": "/supersquad/static/js/runtime-main.63cc95c2.js"
+  },
+  {
+    "revision": "4ddc758d490102bf391845e6f0791725",
+    "url": "/supersquad/static/media/background.4ddc758d.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/supersquad/static/media/logo.25bf045c.svg"
+  }
+]);
